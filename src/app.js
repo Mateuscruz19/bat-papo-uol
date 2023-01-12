262362a08aa3cab7dbbee527aaa0a13feb8bfaa7
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-01-12 20:03:51 -0300

    Feature: Rote /Participants maded

--- src/app.js
+++ src/app.js
@@ -13,36 +13,62 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 try {
-    await mongoClient.connect();
+     mongoClient.connect();
+     db = mongoClient.db();
 } catch (err) {
     console.log("Erro no mongo.conect", err.message);
     }
     
-    db = mongoClient.db("Uol");
     const Participants = db.collection("participants");
     const Maintenance = db.collection("maintenance")
     const Messages = db.collection("Messages")
     const today = Date.now();
 
-app.post("/participants", (req,res) ={
+app.post("/participants", async (req,res) => {
 
+    const { name } = req.body
+
+    if(typeof name != 'string' || !name) return res.status(422)
+
+    try {
+        const participantsExists = await Participants.findOne({ name });
+        if(participantsExists) {
+        return res.sendStatus(409);
+    }
+
+    await Participants.insertOne({ name, lastStatus: today})
+
+    await Messages.insertOne({
+        from: name,
+        to: "Todos",
+        text: "entrei na sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss")
+    })
+
+    res.sendStatus(201);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 })
 
-app.get("/participants", (req,res) ={
 
-})
+// app.get("/participants", (req,res) => {
+    
+// })
 
-app.post("/messages", (req,res) ={
+// app.post("/messages", (req,res) => {
 
-})
+// })
 
-app.get("/messages", (req,res) ={
+// app.get("/messages", (req,res) => {
 
-})
+// })
 
-app.get("/status", (req,res) ={
+// app.get("/status", (req,res) => {
 
-})
+// })
 
 
 
